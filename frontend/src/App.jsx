a90dd889db3b3db2dfcb7c6088a1fc22bda3c314
Esha <esharamakrishnan21@gmail.com>
2025-03-08 00:35:01 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Bankers from './pages/Bankers'
+import Login from './pages/Login'
+import MyProfile from './pages/MyProfile'
+//import { ContainerWithChildren } from 'postcss/lib/container'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1 style={{ color: "blue" }}>Hello from App</h1>
+    </div>
+  );
+};
+
+export default App;
+
+// const App = () => {
+//   return (
+//     <div className='mx-4 sm:mx-[10%]'>
+//       <Navbar>
+//       <Routes>
+//         <Route path='/' element={<Home />} />
+//         <Route path='/Bankers' element={<Bankers />} />
+//         <Route path='/Bankers/:speciality' element={<Bankers />} />
+//         <Route path='/Login' element={<Login />} />
+//         <Route path='/About' element={<About />} />
+//         <Route path='/contact' element={<Contact />} />
+//         <Route path='/My-Profile' element={<MyProfile />} />
+//         <Route path='/My-Appointments' element={<MyAppointments />} />
+//         <Route path='/Appointment/:bankId' element={<Appointment />} />
+
+//       </Routes>
+//       </Navbar>
+//     </div>
+//   )
+// }
+
+// export default App
---
+++ frontend/src/pages/Bankers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bankers = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Bankers
